9eaf09c3aa1d3054944a6b599c4f05d04bc8b724
reactionic127 <richard.gill127@gmail.com>
2020-12-26 02:19:37 +0800

    added react navigation

--- App.tsx
+++ App.tsx
@@ -1,16 +1,24 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
-import {Apartments} from './src/screens/Apartments';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {ApartmentsScreen} from './src/screens/Apartments';
 
 const client = new ApolloClient({
   uri: 'http://localhost:3000/graphql',
   cache: new InMemoryCache(),
 });
+const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <ApolloProvider client={client}>
-      <Apartments />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Apartments" component={ApartmentsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </ApolloProvider>
   );
 };
